10d753807f28843b1ebb8d9e4ce87658f9ad7241
unknown <spoki_93@hotmail.com>
2023-11-29 21:50:52 -0500

    efectos login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,42 +3,58 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Icredenciales } from '../interfaces/icredenciales';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent { 
+export class LoginComponent {
   hide = true;
-  authservices=inject(AuthService)
-  router=inject(Router)
-  fb=inject(FormBuilder)
+  authservices = inject(AuthService)
+  router = inject(Router)
+  fb = inject(FormBuilder)
 
-  form=this.fb.group({
-    usuario:['',Validators.required],
-    password:['',Validators.required]
+  form = this.fb.group({
+    usuario: ['', Validators.required],
+    password: ['', Validators.required]
   })
 
-  obtenerErrorCampoNombre(camponom:string){
-    var campo=this.form.get(camponom)
+  obtenerErrorCampoNombre(camponom: string) {
+    var campo = this.form.get(camponom)
 
-    if(campo?.hasError('required')){
+    if (campo?.hasError('required')) {
       return 'El campo es requerido';
     }
     return ''
   }
 
-  onSubmit(credenciales:Icredenciales){ 
-    this.authservices.login(credenciales).subscribe(data=>{
-      
+  onSubmit(credenciales: Icredenciales) {
+    this.authservices.login(credenciales).subscribe(data => {
+
       this.authservices.guardarToken(data)
       //this.authservices.guardarToken(data);
-      this.router.navigate(['/admin'])
+      this.router.navigate(['/admin/Mapa'])
     },
-    (error)=>{
-      console.log(error.error.message)
-    }
+      (error) => {
+        this.form.reset();
+        let timerInterval;
+        Swal.fire({
+          icon: "error",
+          html: "Se cerrara automaticamente en <b></b> millisegundos.",
+          title: "Usuario y/o contraseña incorrecta.",
+          timer:3000,
+          timerProgressBar: true,
+          text: error.error.message,
+          didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup()?.querySelector("b")!;
+            timerInterval = setInterval(() => {
+              timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+          },
+        })
+      }
     )
   }
 }
